cff8eb81a32e2c588bcace09fc85a8d699755508
Lakshmi <lakshmi.web13@gmail.com>
2023-04-07 22:12:38 +0200

    day01
    Data types, class, static and nonstatic

---
+++ src/day01/Javaprogram1.java
@@ -0,0 +1,44 @@
+package day01;
+
+public class Javaprogram1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+boolean a=true;
+System.out.println(a);
+
+a= false;
+System.out.println(a);
+
+byte a1= -128;
+System.out.println(a1);
+
+short a2= 32767;
+System.out.println(a2);
+
+int a3 = 2345678;
+System.out.println(a3);
+
+a3=a2;
+System.out.println(a3);
+
+long a4 = 12345678890l;
+System.out.println(a4);
+
+float a5 = 234567.5678f;
+System.out.println(a5);
+
+double a6 = 234567.6754378d;
+System.out.println(a6);
+
+char a7 = 'a';
+System.out.println(a7);
+
+String a8 ="123456789sdfghj!@#$%";
+System.out.println(a8);
+
+
+
+	}
+
+}
